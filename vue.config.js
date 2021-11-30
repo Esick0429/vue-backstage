154bb7a9bbdb3fc46d7bcf83f8102d164faef4b7
Reality <2476124695@qq.com>
2021-11-30 16:18:02 +0800

    改了一下proxy代理

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports={
       open: true, // 配置自动启动浏览器
         proxy: {
           '/api': { // 匹配所有以 '/api'开头的请求路径
-            target: 'http://127.0.0.1:4000/api', // 代理目标的基础路径
+            target: 'http://119.91.27.40:4000/api', // 代理目标的基础路径
             changeOrigin: true ,// 支持跨域
             pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
               '^/api': '',
